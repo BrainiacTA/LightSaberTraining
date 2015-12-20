e6df66ef25ad175cfd7d103fa1e41c259c7f428f
kalindimchev <kalin.dimchev@gmail.com>
2015-12-20 16:27:41 +0200

    Add backend architecture

---
+++ LightSaberGame/LightSaberGame/Data/IData.cs
@@ -0,0 +1,16 @@
+﻿using LightSaberGame.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightSaberGame.Data
+{
+    public interface IData
+    {
+        Task<IEnumerable<HeroModel>> GetHeroes();
+
+        HeroModel AddHero(HeroModel model);
+    }
+}
---
+++ LightSaberGame/LightSaberGame/Model/HeroGetResponse.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LightSaberGame.Model
+{
+    public class HeroGetResponse
+    {
+        public IEnumerable<HeroModel> Result { get; set; }
+    }
+}
