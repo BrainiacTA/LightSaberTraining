7789fdda99472983eeff7cb2a6efec4bb282f0cc
PavelNPavlov <pavelnapavlov@gmail.com>
2015-12-20 21:03:17 +0200

    relocation and fixing namespaces

--- LightSaberGame/LightSaberGame/Pages/HighScoresPage.xaml.cs
+++ LightSaberGame/LightSaberGame/Pages/HighScoresPage.xaml.cs
@@ -1,24 +1,11 @@
-﻿using LightSaberGame.Data;
-using LightSaberGame.ViewModels.BaseViewModels;
-using LightSaberGame.ViewModels.GameObjectsViewModels;
-using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Runtime.InteropServices.WindowsRuntime;
-using Windows.Foundation;
-using Windows.Foundation.Collections;
-using Windows.UI.Xaml;
-using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Controls.Primitives;
-using Windows.UI.Xaml.Data;
-using Windows.UI.Xaml.Input;
-using Windows.UI.Xaml.Media;
-using Windows.UI.Xaml.Navigation;
-
-
-namespace LightSaberGame.Pages
+﻿namespace LightSaberGame.Pages
 {
+    using LightSaberGame.Data;
+    using LightSaberGame.ViewModels;
+    using System.Collections.Generic;
+    using Windows.UI.Xaml;
+    using Windows.UI.Xaml.Controls;
+
     public sealed partial class HighScoresPage : Page
     {
         public HighScoresPage()
