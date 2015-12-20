77c8d6f5147de63f2672659c1fefcfab8f9c1933
PavelNPavlov <pavelnapavlov@gmail.com>
2015-12-20 10:25:38 +0200

    Tilted TextAdded

---
+++ LightSaberGame/LightSaberGame/Views/TiltedTextBlockControl.xaml.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace LightSaberGame.Views
+{
+    public sealed partial class TiltedTextBlockControl : UserControl
+    {
+
+
+        public string Text
+        {
+            get { return (string)GetValue(TextProperty); }
+            set { SetValue(TextProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Text.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty TextProperty =
+            DependencyProperty.Register("Text", typeof(string), typeof(TiltedTextBlockControl), new PropertyMetadata(""));
+
+
+
+
+        public double Angle
+        {
+            get { return (double)GetValue(AngleProperty); }
+            set { SetValue(AngleProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for Angle.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty AngleProperty =
+            DependencyProperty.Register("Angle", typeof(double), typeof(TiltedTextBlockControl), new PropertyMetadata(0));
+
+
+        public TiltedTextBlockControl()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
